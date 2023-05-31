905fdb418ed17aec49df2e7e3a8f7f691135b283
lisov.alexandr@mail.ru <lisov.alexandr@mail.ru>
2023-06-01 00:48:56 +0300

    refactoring

---
+++ test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './test'
+
+const FuncTest = () =>{
+  return (
+   <div className='app-wrapper'>
+    <div className='header'>шляпа</div>
+    <div className='navbar'>контент</div>
+    <div className='content'>новигатор</div>
+   </div>
+    )
+}
+
+export default FuncTest
